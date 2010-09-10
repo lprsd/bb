3c14fe8144c0130fdae4f88f6793da47594b3bfd
Lakshman Prasad <lakshman@uswaretech.com>
2010-09-10 16:28:58 +0530

    Well, the basic blog, All else in the branches. Only

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from django.forms import ModelForm
 
-from djangoratings.fields import RatingField
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     datetime = models.DateTimeField()
 
-    rating = RatingField(range=5) # 5 possible rating values, 1-5
-    
     class Meta():
         ordering = ('-datetime',)
 
